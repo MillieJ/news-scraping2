d557591f89344a9f116734cb4adaf8b5f33dead3
MillieJ <milliegich@gmail.com>
2022-01-25 07:34:42 +0300

    added scraping file

---
+++ scraping.js
@@ -0,0 +1,66 @@
+/*const request = require('request');
+const cheerio = require('cheerio');
+const fs = require('fs');
+const writeStream = fs.createWriteStream('post.csv');
+
+// Write Headers
+writeStream.write(`Title,Link,Date \n`);
+
+request('https://www.businessdailyafrica.com', (error, response, html) => {
+  if (!error && response.statusCode == 200) {
+    const $ = cheerio.load(html);
+
+    $('.content-three-quarter').each((i, el) => {
+      const title = $(el)
+        .find('.content-one-quarter small-list')
+        .text()
+        .replace(/\s\s+/g, '');
+      const link = $(el)
+        .find('a')
+        .attr('href');
+      const date = $(el)
+        .find('.post-date')
+        .text()
+        .replace(/,/, '');
+
+      // Write Row To CSV
+      writeStream.write(`${title}, ${link}, ${date} \n`);
+    });
+
+    console.log('Scraping Done...');
+  }
+}); */
+
+
+const request = require('request');
+const cheerio = require('cheerio');
+const fs = require('fs');
+const writeStream = fs.createWriteStream('post.csv');
+
+// Write Headers
+writeStream.write(`Title,Link,Date \n`);
+
+request('https://www.businessdailyafrica.com', (error, response, html) => {
+  if (!error && response.statusCode == 200) {
+    const $ = cheerio.load(html);
+
+    $('.page-box-inner').each((i, el) => {
+      const title = $(el)
+        .find('.article-title')
+        .text()
+        .replace(/\s\s+/g, '');
+      const link = $(el)
+        .find('a')
+        .attr('href');
+      const date = $(el)
+        .find('.byline')
+        .text()
+        .replace(/,/, '');
+
+      // Write Row To CSV
+      writeStream.write(`${title}, ${link}, ${date} \n`);
+    });
+
+    console.log('Scraping Done...');
+  }
+});
